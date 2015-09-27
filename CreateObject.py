f10f3adaa4c71f1ef3dc09c679d65d5800406608
Gladson Manuel <gladsonvm@yahoo.com>
2015-09-27 15:17:03 +0530

    Update CreateObject.py

--- CreateObject.py
+++ CreateObject.py
@@ -1,8 +1,8 @@
 for key, value in request.data.iteritems():
   if value is None:
     print key
-    if getattr(ExistingBooking, key) is not None:
-      value = getattr(ExistingBooking, key)
+    if getattr(ExistingClassInstance, key) is not None:
+      value = getattr(ExistingClassInstance, key)
       print 'VALUE::',value
-  setattr(Bookingupdate, key, value)
-Bookingupdate.save()
+  setattr(UpdatedObject, key, value)
+UpdatedObject.save()
